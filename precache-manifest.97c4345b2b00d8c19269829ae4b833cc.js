c256325f1918bfde41c9573df83c09bd6c78aa00
Sidodus <saheedodulaja@gmail.com>
2020-03-17 03:43:22 +0100

    Updates

---
+++ precache-manifest.97c4345b2b00d8c19269829ae4b833cc.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d59a56ce65abbfcf814752150bca47a",
+    "url": "/proforma-invoice-calculator/index.html"
+  },
+  {
+    "revision": "d3840fc3b750528ebc0d",
+    "url": "/proforma-invoice-calculator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f02be5b36c5ea78ed8de",
+    "url": "/proforma-invoice-calculator/static/css/main.595c8f3f.chunk.css"
+  },
+  {
+    "revision": "d3840fc3b750528ebc0d",
+    "url": "/proforma-invoice-calculator/static/js/2.cf331e7e.chunk.js"
+  },
+  {
+    "revision": "9e3f40ea04bbb9fe4c222c108facde66",
+    "url": "/proforma-invoice-calculator/static/js/2.cf331e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02be5b36c5ea78ed8de",
+    "url": "/proforma-invoice-calculator/static/js/main.9e63280b.chunk.js"
+  },
+  {
+    "revision": "484c357e4c88b5511ca8",
+    "url": "/proforma-invoice-calculator/static/js/runtime-main.812db7e6.js"
+  },
+  {
+    "revision": "36e7a8bfb3f250ddc37493130add89e5",
+    "url": "/proforma-invoice-calculator/static/media/fa-brands-400.36e7a8bf.woff"
+  },
+  {
+    "revision": "4a60307193b4dcf411a958f449b2f599",
+    "url": "/proforma-invoice-calculator/static/media/fa-brands-400.4a603071.svg"
+  },
+  {
+    "revision": "764a8efba3c8a098aa85b4c37b8155a6",
+    "url": "/proforma-invoice-calculator/static/media/fa-brands-400.764a8efb.eot"
+  },
+  {
+    "revision": "8dbeb0ea6cd5f1f5cf6ec094dfbfb96c",
+    "url": "/proforma-invoice-calculator/static/media/fa-brands-400.8dbeb0ea.ttf"
+  },
+  {
+    "revision": "c4af52f53368b81cc3ea577f37f9a916",
+    "url": "/proforma-invoice-calculator/static/media/fa-brands-400.c4af52f5.woff2"
+  },
+  {
+    "revision": "09830468cd0d8b16d6e213b1c888744f",
+    "url": "/proforma-invoice-calculator/static/media/fa-duotone-900.09830468.svg"
+  },
+  {
+    "revision": "2a6dec1227f9970376f578270a642d06",
+    "url": "/proforma-invoice-calculator/static/media/fa-duotone-900.2a6dec12.woff2"
+  },
+  {
+    "revision": "e084f1c8b648d7d45a50b0dae900c8b6",
+    "url": "/proforma-invoice-calculator/static/media/fa-duotone-900.e084f1c8.ttf"
+  },
+  {
+    "revision": "e6a9123b6d6d89911e7a3d7f01e33f82",
+    "url": "/proforma-invoice-calculator/static/media/fa-duotone-900.e6a9123b.eot"
+  },
+  {
+    "revision": "ea3d65a7b9829c9090b0a80c6e260a4a",
+    "url": "/proforma-invoice-calculator/static/media/fa-duotone-900.ea3d65a7.woff"
+  },
+  {
+    "revision": "0037347f16a6dd10447b0b2911b0104c",
+    "url": "/proforma-invoice-calculator/static/media/fa-light-300.0037347f.ttf"
+  },
+  {
+    "revision": "86683af0360cc024c998f00de4c43e9e",
+    "url": "/proforma-invoice-calculator/static/media/fa-light-300.86683af0.woff2"
+  },
+  {
+    "revision": "97468fa17b539584bbe0b351319d66e7",
+    "url": "/proforma-invoice-calculator/static/media/fa-light-300.97468fa1.eot"
+  },
+  {
+    "revision": "d8e14023b11ab348bcd6148a50b7ae85",
+    "url": "/proforma-invoice-calculator/static/media/fa-light-300.d8e14023.woff"
+  },
+  {
+    "revision": "ef7b6ed77b842d9524ffb0f297fcc6d9",
+    "url": "/proforma-invoice-calculator/static/media/fa-light-300.ef7b6ed7.svg"
+  },
+  {
+    "revision": "39bd303fbf945408f7f6e585bc086ee4",
+    "url": "/proforma-invoice-calculator/static/media/fa-regular-400.39bd303f.ttf"
+  },
+  {
+    "revision": "75721d809c7b38cce914a6bd07b0973b",
+    "url": "/proforma-invoice-calculator/static/media/fa-regular-400.75721d80.eot"
+  },
+  {
+    "revision": "983a20d28fbf8fbc27cfeb096f0ebd85",
+    "url": "/proforma-invoice-calculator/static/media/fa-regular-400.983a20d2.woff2"
+  },
+  {
+    "revision": "dd012cb958a95fc77ced4ecbb7c87040",
+    "url": "/proforma-invoice-calculator/static/media/fa-regular-400.dd012cb9.woff"
+  },
+  {
+    "revision": "eab6a454886d3c4f7968c4be69ca5c55",
+    "url": "/proforma-invoice-calculator/static/media/fa-regular-400.eab6a454.svg"
+  },
+  {
+    "revision": "0259646b38bc6f3409fe2567a94c37b8",
+    "url": "/proforma-invoice-calculator/static/media/fa-solid-900.0259646b.svg"
+  },
+  {
+    "revision": "265b724844b97d7427fc4a44c9c2e43d",
+    "url": "/proforma-invoice-calculator/static/media/fa-solid-900.265b7248.woff"
+  },
+  {
+    "revision": "27df36c7e2bec8e3624bed94f02e7aa8",
+    "url": "/proforma-invoice-calculator/static/media/fa-solid-900.27df36c7.ttf"
+  },
+  {
+    "revision": "9b39dfc9bafe5944979aff454bdfe44f",
+    "url": "/proforma-invoice-calculator/static/media/fa-solid-900.9b39dfc9.eot"
+  },
+  {
+    "revision": "e245d7d62db89a9912e17b02bc23e256",
+    "url": "/proforma-invoice-calculator/static/media/fa-solid-900.e245d7d6.woff2"
+  },
+  {
+    "revision": "c116468ad19581149cfe131997218362",
+    "url": "/proforma-invoice-calculator/static/media/nigerianFlag.c116468a.jpg"
+  },
+  {
+    "revision": "8106443d151fd03e11295cc32616abb7",
+    "url": "/proforma-invoice-calculator/static/media/usFlag.8106443d.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/proforma-invoice-calculator/precache-manifest.97c4345b2b00d8c19269829ae4b833cc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/proforma-invoice-calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
