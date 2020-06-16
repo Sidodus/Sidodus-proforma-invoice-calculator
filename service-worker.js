69c5ca88599237e1517fdce68966558d0e3326a6
Saheed Odulaja <saheedodulaja@gmail.com>
2020-06-16 02:44:01 +0100

    Version 3.0.0.1 Update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sidodus-proforma-invoice-calculator/precache-manifest.d63cc602405bb1657df6b588e9172c8c.js"
+  "/Sidodus-proforma-invoice-calculator/precache-manifest.6cea3fd3c43e98997af9d35e5f50e0a4.js"
 );
 
 self.addEventListener('message', (event) => {
