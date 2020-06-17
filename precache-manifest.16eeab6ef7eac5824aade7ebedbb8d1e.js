5c9c76565e050497f0de49b8493d713daa633f8c
Saheed Odulaja <saheedodulaja@gmail.com>
2020-06-17 16:38:00 +0100

    Version 3.0.1 Update

---
+++ precache-manifest.16eeab6ef7eac5824aade7ebedbb8d1e.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dadeccf3fd475d2af674c43439157b47",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/index.html"
+  },
+  {
+    "revision": "1072107f8d432c630cbf",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "116d772b8413933b0589",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/css/main.9069bcb0.chunk.css"
+  },
+  {
+    "revision": "1072107f8d432c630cbf",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/js/2.4159a141.chunk.js"
+  },
+  {
+    "revision": "aeb6b778806866754ca3a228056e794d",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/js/2.4159a141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116d772b8413933b0589",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/js/main.477607c4.chunk.js"
+  },
+  {
+    "revision": "ad9b51f5b86b6371fc2c",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/js/runtime-main.358b6f4d.js"
+  },
+  {
+    "revision": "36e7a8bfb3f250ddc37493130add89e5",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-brands-400.36e7a8bf.woff"
+  },
+  {
+    "revision": "4a60307193b4dcf411a958f449b2f599",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-brands-400.4a603071.svg"
+  },
+  {
+    "revision": "764a8efba3c8a098aa85b4c37b8155a6",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-brands-400.764a8efb.eot"
+  },
+  {
+    "revision": "8dbeb0ea6cd5f1f5cf6ec094dfbfb96c",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-brands-400.8dbeb0ea.ttf"
+  },
+  {
+    "revision": "c4af52f53368b81cc3ea577f37f9a916",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-brands-400.c4af52f5.woff2"
+  },
+  {
+    "revision": "09830468cd0d8b16d6e213b1c888744f",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-duotone-900.09830468.svg"
+  },
+  {
+    "revision": "2a6dec1227f9970376f578270a642d06",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-duotone-900.2a6dec12.woff2"
+  },
+  {
+    "revision": "e084f1c8b648d7d45a50b0dae900c8b6",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-duotone-900.e084f1c8.ttf"
+  },
+  {
+    "revision": "e6a9123b6d6d89911e7a3d7f01e33f82",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-duotone-900.e6a9123b.eot"
+  },
+  {
+    "revision": "ea3d65a7b9829c9090b0a80c6e260a4a",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-duotone-900.ea3d65a7.woff"
+  },
+  {
+    "revision": "0037347f16a6dd10447b0b2911b0104c",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-light-300.0037347f.ttf"
+  },
+  {
+    "revision": "86683af0360cc024c998f00de4c43e9e",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-light-300.86683af0.woff2"
+  },
+  {
+    "revision": "97468fa17b539584bbe0b351319d66e7",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-light-300.97468fa1.eot"
+  },
+  {
+    "revision": "d8e14023b11ab348bcd6148a50b7ae85",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-light-300.d8e14023.woff"
+  },
+  {
+    "revision": "ef7b6ed77b842d9524ffb0f297fcc6d9",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-light-300.ef7b6ed7.svg"
+  },
+  {
+    "revision": "39bd303fbf945408f7f6e585bc086ee4",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-regular-400.39bd303f.ttf"
+  },
+  {
+    "revision": "75721d809c7b38cce914a6bd07b0973b",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-regular-400.75721d80.eot"
+  },
+  {
+    "revision": "983a20d28fbf8fbc27cfeb096f0ebd85",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-regular-400.983a20d2.woff2"
+  },
+  {
+    "revision": "dd012cb958a95fc77ced4ecbb7c87040",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-regular-400.dd012cb9.woff"
+  },
+  {
+    "revision": "eab6a454886d3c4f7968c4be69ca5c55",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-regular-400.eab6a454.svg"
+  },
+  {
+    "revision": "0259646b38bc6f3409fe2567a94c37b8",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-solid-900.0259646b.svg"
+  },
+  {
+    "revision": "265b724844b97d7427fc4a44c9c2e43d",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-solid-900.265b7248.woff"
+  },
+  {
+    "revision": "27df36c7e2bec8e3624bed94f02e7aa8",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-solid-900.27df36c7.ttf"
+  },
+  {
+    "revision": "9b39dfc9bafe5944979aff454bdfe44f",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-solid-900.9b39dfc9.eot"
+  },
+  {
+    "revision": "e245d7d62db89a9912e17b02bc23e256",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/fa-solid-900.e245d7d6.woff2"
+  },
+  {
+    "revision": "c116468ad19581149cfe131997218362",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/nigerianFlag.c116468a.jpg"
+  },
+  {
+    "revision": "8106443d151fd03e11295cc32616abb7",
+    "url": "/Sidodus/Sidodus-proforma-invoice-calculator/static/media/usFlag.8106443d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sidodus-proforma-invoice-calculator/precache-manifest.6cea3fd3c43e98997af9d35e5f50e0a4.js"
+  "/Sidodus/Sidodus-proforma-invoice-calculator/precache-manifest.16eeab6ef7eac5824aade7ebedbb8d1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sidodus-proforma-invoice-calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Sidodus/Sidodus-proforma-invoice-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
